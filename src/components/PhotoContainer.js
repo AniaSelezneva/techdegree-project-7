849ec59aaf6569114e11244d5a379ce8d72bdd3d
Anna S <bamguanat@yandex.ru>
2019-11-03 17:32:55 +0300

    Update PhotoContainer.js

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,33 +1,26 @@
-import React from 'react';
-import NotFound from './NoResult';
-import Photo from './Photo';
+import React, { Component } from "react";
+import Photo from "./Photo";
+import Loader from "../loader.gif";
 
-const PhotoContainer = props => {
-  const results = props.data;
-  let photos;
-  
-  if (typeof results.photos != "undefined"){
-    if (typeof results.photos.photo != "undefined"){
-      if (results.photos.photo.length > 0) {
-        photos = results.photos.photo.map (result => 
-          <Photo url={(`https://farm${result.farm}.staticflickr.com/${result.server}/${result.id}_${result.secret}_m.jpg`)} key={result.id} />
-      )} else {
-        photos = <NotFound />
-      }
-    }
+class PhotoContainer extends Component {
+  render() {
+    return (
+      <div className="photo-container">
+        {/*Loader*/}
+        <img
+          src={Loader}
+          className={`search-loading ${this.props.loading ? "show" : "hide"}`}
+          alt="Loader"
+        />
+        <h2>{this.props.totalResults > 0 ? this.props.query : ""}</h2>
+        <ul>
+          {this.props.urls.map(url => (
+            <Photo src={url} key={this.props.urls.indexOf(url)} />
+          ))}
+        </ul>
+      </div>
+    );
   }
-
-  return (
-    <div className="photo-container">
-      <h2>
-       {/* {props.SearchValue} */}
-        Results
-      </h2>
-      <ul>
-        {photos}
-      </ul>
-    </div>
-  )
 }
 
 export default PhotoContainer;
