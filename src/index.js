ad4e3affa131983010a356d30dec52192561d2a9
Anna S <bamguanat@yandex.ru>
2019-11-03 17:33:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./Components/App.jsx";
 
-ReactDOM.render (
-    <App />,
-    document.querySelector('#root')
-  );
-  
+ReactDOM.render(<App />, document.querySelector(".container"));
